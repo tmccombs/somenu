e7b2f173ccc59dd80fdc046d09b1ebcb76bb0638
Thayne McCombs <astrothayne@gmail.com>
2018-11-17 02:16:26 -0700

    Get it working

--- somenu.py
+++ somenu.py
@@ -10,7 +10,8 @@ import sys
 
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, Gio
+from gi.repository import Gtk, Gio, GLib
+import os.path
 import xdg.Menu
 
 MENU_CONFIG = "/etc/xdg/menus/applications.menu"
@@ -18,67 +19,109 @@ MENU_CONFIG = "/etc/xdg/menus/applications.menu"
 def build_menu(model):
     'Build a Gtk.Menu from an xdg.Menu.Menu'
     view = Gtk.Menu()
-    for item in model.getEntries():
-        if isinstance(item, xdg.Menu.Menu):
-            add_submenu(view, item)
-        elif isinstance(item, xdg.Menu.MenuEntry):
-            add_entry(view, item.DesktopEntry)
-        elif isinstance(item, xdg.Menu.Separator):
-            seperator = Gtk.SeparatorMenuItem.new()
-            view.append(seperator)
-            seperator.show()
+    for entry in model.getEntries():
+        if not entry.Show:
+            continue
+        if isinstance(entry, xdg.Menu.Menu):
+            item = create_submenu(entry)
+        elif isinstance(entry, xdg.Menu.MenuEntry):
+            item = create_entry(entry.DesktopEntry)
+        elif isinstance(entry, xdg.Menu.Separator):
+            item = Gtk.SeparatorMenuItem.new()
+        view.append(item)
     view.show_all()
     return view
 
-def append_menu_item(menu, label, icon_name, comment):
-    'Add an item to a Gtk.Menu'
-    item = Gtk.MenuItem(label=label)
-    # TODO icon
+def build_menu_item(label, icon_name, comment):
+    'Create a Gtk.Menu'
+    item = Gtk.MenuItem()
+    box = Gtk.Box()
+    image = get_image(icon_name)
+    if image:
+        box.pack_start(get_image(icon_name), False, False, 0)
+    box.pack_end(Gtk.Label.new(label), True, True, 0)
+    item.add(box)
     if comment is not None:
         item.set_tooltip_text(comment)
-    menu.append(item)
     return item
 
-def add_entry(menu, entry):
-    'Add an item to Gtk.Menu for an xdg.Menu.MenuEntry'
-    view = append_menu_item(menu, entry.getName(), entry.getIcon(), entry.getComment())
-    view.connect('activate', on_execute, entry)
+def create_entry(entry):
+    'Create a Gtk.MenuItem for an xdg.Menu.MenuEntry'
+    item = build_menu_item(entry.getName(), entry.getIcon(), entry.getComment())
+    item.connect('activate', on_execute, entry)
+    return item
 
-def add_submenu(menu, model):
+def create_submenu(model):
     'Add a submenu to a Gtk.Menu for an xdg.Menu.Menu'
     submenu = build_menu(model)
-    item = append_menu_item(menu, model.getName(), model.getIcon(), model.getComment())
+    item = build_menu_item(model.getName(), model.getIcon(), model.getComment())
     item.set_submenu(submenu)
+    return item
+
+def get_image(icon_name):
+    if not icon_name:
+        return None
+    try:
+        # FIXME: fix icon size
+        if os.path.isabs(icon_name):
+            image = Gtk.Image.new_from_file(icon_name)
+            image.set_pixel_size(16)
+            return image
+        return Gtk.Image.new_from_icon_name(icon_name, Gtk.IconSize.MENU)
+    except:
+        print("Failed to get icon for {}".format(icon_name))
+        # should we use a placeholder icon?
+        return None
 
 def on_execute(widget, entry):
     'Callback for clicking on a menu entry'
-    # TODO: better launch
-    print("should run: {}".format(entry.getExec()))
+    app_info = Gio.DesktopAppInfo.new_from_filename(entry.filename)
+    app_info.launch()
 
 class MenuApplication(Gio.Application):
     '''The application to show the menu'''
 
     def __init__(self):
         '''Initialize the application'''
-        print("a")
-        Gio.Application.__init__( self, application_id="com.github.tmccombs.somenu")
-        print("b")
-        self.menu = None
+        Gio.Application.__init__(self, application_id="com.github.tmccombs.somenu", flags=Gio.ApplicationFlags.HANDLES_COMMAND_LINE)
+        self.add_main_option('file',
+                             ord('f'),
+                             GLib.OptionFlags.NONE, GLib.OptionArg.FILENAME,
+                             "XDG Menu file to read menu config from")
+        self.add_main_option('prefix',
+                             ord('p'),
+                             GLib.OptionFlags.NONE, GLib.OptionArg.STRING,
+                             "Prefix to use to find XDG Menu file")
+        self.menus = {}
 
     def do_startup(self):
         self.hold()
-        print("startup")
-        #Gtk.Application.do_startup(self)
-        print("creating")
+        Gio.Application.do_startup(self)
 
-        # create a menu
-        self.menu = build_menu(xdg.Menu.parse(MENU_CONFIG))
 
-    def do_activate(self):
-        print("activating ({})".format(self.menu))
-        self.menu.popup(None, None, None, None, 0, Gtk.get_current_event_time())
+    def do_command_line(self, cl):
+        options = cl.get_options_dict()
+        prefix_opt = options.lookup_value('prefix')
+        file_opt = options.lookup_value('file')
+        if file_opt:
+            conf_file = str(file_opt.get_bytestring())
+        elif prefix_opt:
+            conf_file = '/etc/xdg/menus/{}-applications.menu'.format(prefix_opt.get_string())
+        else:
+            conf_file = '/etc/xdg/menus/applications.menu'
+        print("conf_file ", conf_file)
+        if conf_file not in self.menus:
+            print("building menu for {}".format(conf_file))
+            self.menus[conf_file] = build_menu(xdg.Menu.parse(conf_file))
+        self.menus[conf_file].popup(None, None, None, None, 0, Gtk.get_current_event_time())
 
-if __name__ == '__main__':
+        Gio.Application.do_command_line(self, cl)
+
+def main():
+    "Entrance point"
     app = MenuApplication()
     exit_status = app.run(sys.argv)
     sys.exit(exit_status)
+
+if __name__ == '__main__':
+    main()
